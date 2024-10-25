dcda0e5acd9ff89237c6979c2cd7dc7aa8491b7f
Shankar-Kanni <87437598+Shankar-Kanni@users.noreply.github.com>
2024-10-25 10:05:17 +0200

    alert

--- terraform.tf
+++ terraform.tf
@@ -3,19 +3,40 @@ provider "google" {
     region = "us-central1" 
 }
 
-resource "google_compute_instance" "vm_instance" {
-    name         = "gcptutorials-vm"
-    machine_type = "e2-small"
-    zone = "us-central1-a"
+# resource "google_compute_instance" "vm_instance" {
+#     name         = "gcptutorials-vm"
+#     machine_type = "e2-small"
+#     zone = "us-central1-a"
 
-    boot_disk {
-    initialize_params {
-        image = "debian-cloud/debian-11"
+#     boot_disk {
+#     initialize_params {
+#         image = "debian-cloud/debian-11"
+#     }
+#     }    
+#     network_interface {       
+#     network = "default"
+#     access_config {
+#     }
+#     }
+# }
+
+resource "google_monitoring_alert_policy" "alert_policy" {
+  display_name = "My Alert Policy"
+  combiner     = "OR"
+  conditions {
+    display_name = "test condition"
+    condition_threshold {
+      filter     = "metric.type=\"compute.googleapis.com/instance/disk/write_bytes_count\" AND resource.type=\"gce_instance\""
+      duration   = "60s"
+      comparison = "COMPARISON_GT"
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
     }
-    }    
-    network_interface {       
-    network = "default"
-    access_config {
-    }
-    }
+  }
+
+  user_labels = {
+    foo = "bar"
+  }
 }
